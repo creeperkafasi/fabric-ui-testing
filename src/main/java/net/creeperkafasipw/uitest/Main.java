a39b7c43ed9b8580a4e2a2ba27133aee99c9fc3c
creeperkafasipw <49831545+creeperkafasi@users.noreply.github.com>
2020-10-31 17:02:08 +0300

    basic fabric mod stuff

---
+++ src/main/java/net/creeperkafasipw/uitest/Main.java
@@ -0,0 +1,12 @@
+package net.creeperkafasipw.uitest;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer{
+
+    @Override
+    public void onInitialize() {
+        System.out.println("creeperkafasipw ui test mod Main.java");
+    }
+    
+}
